a00b5e540b9af6816c107aa17e77602ca2f28abc
Ryan Busby <BusbyActual@gmail.com>
2016-10-31 21:15:37 -0700

    Switched to bactics and added in answers

---
+++ src/Prompts/Questions.js
@@ -0,0 +1,14 @@
+// Until I built this into a fullstack app I will be storing questions in an object
+
+var questions = {
+ 0: `Write a mul function which will produce the following outputs when invoked:
+
+javascript console.log(mul(2)(3)(4)); // output : 24 console.log(mul(4)(3)(4)); // output : 48`,
+ 1: `What is the difference between .call() and .apply()? Provide an example.
+
+
+
+ `
+
+
+};
--- src/Verbalization/Questions.js
+++ src/Verbalization/Questions.js
@@ -1,11 +1,17 @@
 // Until I built this into a fullstack app I will be storing questions in an object
 
 var questions = {
-   0: 'What is a potential pitfall with using typeof bar === "object" to determine if bar is an object? How can this pitfall be avoided?',
-   1: 'What is the difference between undefined and not defined in JavaScript?',
-   2: 'Can you explain closure in JavaScript? Provide and example.',
-   3: 'How do you check if an object is an array or not?',
-   4: 'What is hoisting in JavaScript and why is it important?',
-   5: 'Can you explain = vs == vs === in JavaScript?'
+   0: `What is a potential pitfall with using typeof bar === "object" to determine if bar is an object? How can this pitfall be avoided?`,
+   1: `What is the difference between undefined and not defined in JavaScript?`,
+   2: `Can you explain closure in JavaScript? Provide and example.`,
+   3: `How do you check if an object is an array or not?`,
+   4: `What is hoisting in JavaScript and why is it important?`,
+   5: `Can you explain = vs == vs === in JavaScript?`
 };
+
+
+var answers = {
+  0: `The potential pitfal is there are many constuctors that inherit from the object constructor. For example typof [] === "object" will return true. It can present false positives under these circumstances.`,
+  1: `Undfined means often a variable may be declared but not assigned a value. Not defined means that in the scope or context of execution something needing to be defined is not present.`
+}
 
