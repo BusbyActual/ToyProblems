b5a62b31681f1020b004c5fe34d24eea7ff84569
Ryan Busby <BusbyActual@gmail.com>
2016-10-31 18:45:55 -0700

    Constructing an object to store questions interim fullstackification : ^ )

--- src/Verbalization/Questions.js
+++ src/Verbalization/Questions.js
@@ -1,1 +1,7 @@
-// What is a potential pitfall with using typeof bar === "object" to determine if bar is an object? How can this pitfall be avoided?
+//Until I built this into a fullstack app I will be storing questions in an object
+
+var questions = {
+   0: 'What is a potential pitfall with using typeof bar === "object" to determine if bar is an object? How can this pitfall be avoided?',
+   1: 'What is the difference between undefined and not defined in JavaScript?'
+};
+
