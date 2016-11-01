239070405cfeb2518da3a4bea1606e396ff1f7a5
Ryan Busby <BusbyActual@gmail.com>
2016-10-31 20:50:47 -0700

    Added questions and added folders for js prompts

--- src/Verbalization/Questions.js
+++ src/Verbalization/Questions.js
@@ -1,7 +1,11 @@
-//Until I built this into a fullstack app I will be storing questions in an object
+// Until I built this into a fullstack app I will be storing questions in an object
 
 var questions = {
    0: 'What is a potential pitfall with using typeof bar === "object" to determine if bar is an object? How can this pitfall be avoided?',
-   1: 'What is the difference between undefined and not defined in JavaScript?'
+   1: 'What is the difference between undefined and not defined in JavaScript?',
+   2: 'Can you explain closure in JavaScript? Provide and example.',
+   3: 'How do you check if an object is an array or not?',
+   4: 'What is hoisting in JavaScript and why is it important?',
+   5: 'Can you explain = vs == vs === in JavaScript?'
 };
 
