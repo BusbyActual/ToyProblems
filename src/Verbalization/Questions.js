848277e652d3a0415bf1bd1f183284afffcbf08f
Ryan Busby <BusbyActual@gmail.com>
2016-10-31 21:21:00 -0700

    added more answerswq

--- src/Verbalization/Questions.js
+++ src/Verbalization/Questions.js
@@ -12,6 +12,10 @@ var questions = {
 
 var answers = {
   0: `The potential pitfal is there are many constuctors that inherit from the object constructor. For example typof [] === "object" will return true. It can present false positives under these circumstances.`,
-  1: `Undfined means often a variable may be declared but not assigned a value. Not defined means that in the scope or context of execution something needing to be defined is not present.`
+  1: `Undfined means often a variable may be declared but not assigned a value. Not defined means that in the scope or context of execution something needing to be defined is not present.`,
+  2: `Closure is when an outer function has access to the values of an inner function even after the inner function has returned. An example might be to push the result of a function to an array so the outer function may do something with it.`,
+  3: `You can check if an object is an array or not with Array.isArray(value). It's a native method on the Array constructor.`,
+  4: `Hoisting is where a function or variable declared is hoited to the top of the current scope. That being inside of a function or perhaps the global scope. It's important because values in JavaScript need to be properly declared when one would need access of them. Most variables are declared at the top of the scope for this reason.`,
+  5: ``
 }
 
